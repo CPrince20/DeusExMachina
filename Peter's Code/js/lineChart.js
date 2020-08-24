41fdad1fb7a7543a7307aaaa39498e07f66872a0
peterluzl <61099859+peterluzl@users.noreply.github.com>
2020-08-24 11:44:03 -0700

    Updated code

--- Peter's Code/js/lineChart.js
+++ Peter's Code/js/lineChart.js
@@ -61,7 +61,7 @@ async function initLineChart() {
           .map(item => item[1])
           .map(item => ({
           ...item,
-          Date: d3.timeParse('%Y-%m-%d')(`${item.Year}-${item.Month}-${1}`),
+          Date: d3.timeParse('%Y-%m-%d')(`${item.Year}-${1}-${1}`),
           AvgTemperature: Math.floor(item.sum / item.totalDay)
         }))
         break;
@@ -76,17 +76,27 @@ async function initLineChart() {
       .attr('transform',
         'translate(' + margin.left + ',' + margin.top + ')');
 
-    var x = d3.scaleTime()
-      .domain(d3.extent(temp, function (d) {
-        return d.Date;
-      }))
+    var x = d3.scaleLinear()
+      .domain([
+        d3.min(temp, function (d) {
+          return +d.Year;
+        }),
+        d3.max(temp, function (d) {
+          return +d.Year;
+        })
+      ])
       .range([0, width]);
 
     svg.append('g')
       .attr('transform', 'translate(0,' + height + ')')
       .call(d3.axisBottom(x)
-        .ticks(d3.timeYear.every(1))
+        .ticks(d3.max(temp, function (d) {
+            return +d.Year;
+          }) - d3.min(temp, function (d) {
+            return +d.Year;
+          }))
         .tickSize(-width)
+        .tickFormat(function (d, i) { console.log(d); return d })
       )
       .call(g => g.selectAll(".tick:not(:first-of-type) line")
         .attr("stroke-opacity", 0.5)
@@ -105,7 +115,7 @@ async function initLineChart() {
         30,
         d3.max(temp, function (d) {
           return +d.AvgTemperature;
-        })])
+        }) + 10])
       .range([height, 0]);
 
     svg.append('g')
@@ -114,14 +124,12 @@ async function initLineChart() {
       )
       .call(g => g.selectAll(".tick:not(:first-of-type) line")
         .attr("stroke-opacity", 0.5)
-        .attr("stroke-dasharray", "2,2"))
+        .attr("stroke-dasharray", "2,2")
+      )
       .call(g => g.selectAll(".tick text")
-        .style('font-size', '16px'))
-      // .call(g => g.selectAll(".tick text")
-      //   .attr("x", 4)
-      //   .attr("dy", -4))
+        .style('font-size', '16px')
+      )
 
-    // Add the line
     svg.append('path')
       .datum(temp)
       .attr('fill', 'none')
@@ -129,7 +137,7 @@ async function initLineChart() {
       .attr('stroke-width', 2.5)
       .attr('d', d3.line()
         .x(function (d) {
-          return x(d.Date)
+          return x(d.Year)
         })
         .y(function (d) {
           return y(d.AvgTemperature)
@@ -151,6 +159,61 @@ async function initLineChart() {
         (height + margin.top + 30) + ")")
       .style("text-anchor", "middle")
       .text("Date");
+
+    var focus = d3.select(`.${type} svg`)
+      .append("g")
+      .attr("class", "focus")
+      .style("display", "none");
+
+    focus.append("circle")
+      .attr("r", 5);
+
+    focus.append("rect")
+      .attr("class", "tooltip")
+      .attr("width", 150)
+      .attr("height", 50)
+      .attr("x", 10)
+      .attr("y", -22)
+      .attr("rx", 4)
+      .attr("ry", 4);
+
+    focus.append("text")
+      .attr("class", "tooltip-date")
+      .attr("x", 18)
+      .attr("y", -2);
+
+    focus.append("text")
+      .attr("x", 18)
+      .attr("y", 18)
+      .text("Temperature:");
+
+    focus.append("text")
+      .attr("class", "tooltip-temperature")
+      .attr("x", 120)
+      .attr("y", 18);
+
+    svg.append("rect")
+      .attr("class", "overlay")
+      .attr("width", width)
+      .attr("height", height)
+      .on("mouseover", function() { focus.style("display", null); })
+      .on("mouseout", function() { focus.style("display", "none"); })
+      .on("mousemove", mousemove);
+
+    function mousemove() {
+      var bisectDate = d3.bisector(function(d) { return d.Year; }).left;
+
+      var x0 = x.invert(d3.mouse(this)[0]),
+        i = bisectDate(temp, x0, 1),
+        d0 = temp[i - 1],
+        d1 = temp[i],
+        d = x0 - d0.Year > d1.Year - x0 ? d1 : d0;
+      // focus.attr("transform", "translate(" + x(d.Year) + 60  + "," + y(d.AvgTemperature) - 30 + ")");
+      focus.attr("transform", `translate(${x(d.Year) + 60}, ${y(d.AvgTemperature) + 10})`);
+      focus.select(".tooltip-date").text(d.Year);
+      focus.select(".tooltip-temperature").text(d.AvgTemperature);
+    }
+
   }
 
   function addInputEvent() {
